7c399f70665031a9225aa7f2b9af3771272e07e2
Nursadyk <tugolbajuulunursadyk@gmail.com>
2024-03-27 14:13:08 +0600

    main-page

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__inner">
+          <Link to="/" className="logo">
+            FLOR<span className="sub-logo">ELLO</span>
+          </Link>
+          <div className="header__right">
+            <nav className="navbar">
+              <ul className="navbar__list">
+                <li className="navbar__item">Home</li>
+                <li className="navbar__item">Shop</li>
+                <li className="navbar__item">Admin</li>
+              </ul>
+            </nav>
+            <div className="header__icons">
+              <button className="header__search-btn">
+                <span class="material-symbols-outlined">search</span>
+              </button>
+              <button className="header__favorite-btn">
+                <span class="material-symbols-outlined">favorite</span>
+              </button>
+              <button className="header__basket-btn">
+                <span class="material-symbols-outlined">shopping_bag</span>
+              </button>
+              <span>CART $0</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
